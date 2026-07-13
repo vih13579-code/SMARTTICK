7a682aedad79a7ba7851936671c1306d7c529fe8
vothihuetran6666-commits <vothihuetran6666@gmail.com>
2026-07-14 01:29:23 +0700

    TranVTH_Login_GoogleAccount

--- src/main/java/Models/GoogleAccount.java
+++ src/main/java/Models/GoogleAccount.java
@@ -1,11 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Models;
 
-import java.io.Serializable;
+import com.google.gson.annotations.SerializedName;
 
 /**
- * Skeleton model/entity: GoogleAccount.
- * TODO: Khai báo fields, constructors, getters/setters theo database schema.
+ *
+ * @author TranVTH
  */
-public class GoogleAccount implements Serializable {
-    private static final long serialVersionUID = 1L;
+
+public class GoogleAccount {
+
+    @SerializedName(value = "sub", alternate = {"id"})
+    private String id;
+    private String email;
+    private String name;
+    private String first_name;
+    private String given_name;
+    private String family_name;
+    private String picture;
+
+    @SerializedName(value = "email_verified", alternate = {"verified_email"})
+    private boolean verified_email;
+
+    public GoogleAccount() {
+    }
+
+    public GoogleAccount(String id, String email, String name, String first_name, String given_name, String family_name, String picture, boolean verified_email) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.first_name = first_name;
+        this.given_name = given_name;
+        this.family_name = family_name;
+        this.picture = picture;
+        this.verified_email = verified_email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getGiven_name() {
+        return given_name;
+    }
+
+    public String getFamily_name() {
+        return family_name;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public boolean isVerified_email() {
+        return verified_email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setGiven_name(String given_name) {
+        this.given_name = given_name;
+    }
+
+    public void setFamily_name(String family_name) {
+        this.family_name = family_name;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public void setVerified_email(boolean verified_email) {
+        this.verified_email = verified_email;
+    }
+
+    @Override
+    public String toString() {
+        return "GoogleAccount{" + "id=" + id + ", email=" + email + ", name=" + name + ", first_name=" + first_name + ", given_name=" + given_name + ", family_name=" + family_name + ", picture=" + picture + ", verified_email=" + verified_email + '}';
+    }
+
 }
