b0065e0900096f5c35e09401904b689fbce4bcc6
Văn Thương <thuong200422@gmail.com>
2026-07-13 15:38:56 +0700

    ThuongNV_View Stock-In Statistics_Statistic

--- src/main/java/Models/Statistic.java
+++ src/main/java/Models/Statistic.java
@@ -1,11 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Models;
 
-import java.io.Serializable;
-
 /**
- * Skeleton model/entity: Statistic.
- * TODO: Khai báo fields, constructors, getters/setters theo database schema.
+ *
+ * @author Thuongnvce181966
  */
-public class Statistic implements Serializable {
-    private static final long serialVersionUID = 1L;
+
+public class Statistic {
+
+    private String productName;
+    private int soldQuantity;
+    private String customerName;
+    private String customerEmail;
+    private int successfulOrders;
+    private String modelName;
+    private int stockQuantity;
+
+    public Statistic(String customerName, String customerEmail, int successfulOrders) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.successfulOrders = successfulOrders;
+    }
+
+    public Statistic(String productName, int soldQuantity) {
+        this.productName = productName;
+        this.soldQuantity = soldQuantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getSoldQuantity() {
+        return soldQuantity;
+    }
+
+    public void setSoldQuantity(int soldQuantity) {
+        this.soldQuantity = soldQuantity;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public int getSuccessfulOrders() {
+        return successfulOrders;
+    }
+
+    public void setSuccessfulOrders(int successfulOrders) {
+        this.successfulOrders = successfulOrders;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
 }
