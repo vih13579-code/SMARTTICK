bc3d35541171f469fb53f511fcdf44c830653e62
LamVHCE171024 <lamvhce171024@fpt.edu.vn>
2026-07-14 00:12:16 +0700

    LamVH_Manage Products for Homepage_Models_AttributeDetail

--- src/main/java/Models/AttributeDetail.java
+++ src/main/java/Models/AttributeDetail.java
@@ -1,11 +1,67 @@
-package Models;
-
-import java.io.Serializable;
-
-/**
- * Skeleton model/entity: AttributeDetail.
- * TODO: Khai báo fields, constructors, getters/setters theo database schema.
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-public class AttributeDetail implements Serializable {
-    private static final long serialVersionUID = 1L;
+package Models;
+/**
+ * 
+ * @author LamVH
+ */
+public class AttributeDetail {
+
+    private int attributeId;
+    private int productId;
+    private String attributeInfor;
+    private String attributeName;
+
+    public AttributeDetail() {
+    }
+
+    public AttributeDetail(int attributeId, int productId, String attributeInfor, String attributeName) {
+        this.attributeId = attributeId;
+        this.productId = productId;
+        this.attributeInfor = attributeInfor;
+        this.attributeName = attributeName;
+    }
+
+    public AttributeDetail(int attributeId, int productId, String attributeInfor) {
+        this.attributeId = attributeId;
+        this.productId = productId;
+        this.attributeInfor = attributeInfor;
+    }
+    
+    
+
+    public int getAttributeId() {
+        return attributeId;
+    }
+
+    public void setAttributeId(int attributeId) {
+        this.attributeId = attributeId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getAttributeInfor() {
+        return attributeInfor;
+    }
+
+    public void setAttributeInfor(String attributeInfor) {
+        this.attributeInfor = attributeInfor;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public void setAttributeName(String attributeName) {
+        this.attributeName = attributeName;
+    }
+
 }
