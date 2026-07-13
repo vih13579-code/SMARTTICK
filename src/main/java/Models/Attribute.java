2dbe35819cc3f35064d6de9081fae28a8c2e02f6
LamVHCE171024 <lamvhce171024@fpt.edu.vn>
2026-07-14 00:10:55 +0700

    LamVH_Manage Products for Homepage_Models_Attribute

--- src/main/java/Models/Attribute.java
+++ src/main/java/Models/Attribute.java
@@ -1,11 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ * @author LamVH
+ */
 package Models;
 
-import java.io.Serializable;
+public class Attribute {
 
-/**
- * Skeleton model/entity: Attribute.
- * TODO: Khai báo fields, constructors, getters/setters theo database schema.
- */
-public class Attribute implements Serializable {
-    private static final long serialVersionUID = 1L;
+    private int attributeId;
+    private int categoryId;
+    private String attributeName;
+
+    public Attribute() {
+    }
+
+    public Attribute(int attributeId, int categoryId, String attributeName) {
+        this.attributeId = attributeId;
+        this.categoryId = categoryId;
+        this.attributeName = attributeName;
+    }
+
+    public int getAttributeId() {
+        return attributeId;
+    }
+
+    public void setAttributeId(int attributeId) {
+        this.attributeId = attributeId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public void setAttributeName(String atrributeName) {
+        this.attributeName = atrributeName;
+    }
+
 }
