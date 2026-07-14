bfc3da57d09276200db9b58171882efdbd8a3fe3
Văn Thương <thuong200422@gmail.com>
2026-07-14 12:28:48 +0700

    ThuongNV_View Stock-In Details_ImportOrderDetail

--- src/main/java/Models/ImportOrderDetail.java
+++ src/main/java/Models/ImportOrderDetail.java
@@ -1,11 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Models;
 
-import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 /**
- * Skeleton model/entity: ImportOrderDetail.
- * TODO: Khai báo fields, constructors, getters/setters theo database schema.
+ *
+ * @author Thuongnvce181966
  */
-public class ImportOrderDetail implements Serializable {
-    private static final long serialVersionUID = 1L;
+
+public class ImportOrderDetail {
+
+    private int ioid;
+    private Product product;
+    private int quantity;
+    private long importPrice;
+
+    public ImportOrderDetail() {
+    }
+
+    public ImportOrderDetail(int ioid, Product product, int quantity, long importPrice) {
+        this.ioid = ioid;
+        this.product = product;
+        this.quantity = quantity;
+        this.importPrice = importPrice;
+    }
+
+    public int getIoid() {
+        return ioid;
+    }
+
+    public void setIoid(int ioid) {
+        this.ioid = ioid;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getImportPrice() {
+        return importPrice;
+    }
+
+    public void setImportPrice(long importPrice) {
+        this.importPrice = importPrice;
+    }
+
+    public String getPriceFormatted() {
+        Locale vietnam = new Locale("vi", "VN");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(vietnam);
+        return currencyFormatter.format(importPrice);
+    }
 }
