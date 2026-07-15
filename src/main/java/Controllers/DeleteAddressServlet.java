b898f664a4fe6783156f52130662e4c73ff4f610
vothihuetran6666-commits <vothihuetran6666@gmail.com>
2026-07-16 01:37:21 +0700

    TranVTH_Manage Address_DeleteAddressServlet

--- src/main/java/Controllers/DeleteAddressServlet.java
+++ src/main/java/Controllers/DeleteAddressServlet.java
@@ -1,26 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
 package Controllers;
 
+import DAOs.AddressDAO;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Skeleton controller for module/feature: DeleteAddressServlet.
- * TODO: Thành viên phụ trách chức năng này tự thêm DAO/Service/Model/View tương ứng.
+ *
+ * @author TranVTH
  */
+
+@WebServlet(name = "DeleteAddressServlet", urlPatterns = {"/DeleteAddress"})
 public class DeleteAddressServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        response.getWriter().println("<h2>DeleteAddressServlet</h2><p>Skeleton servlet - TODO implement feature.</p>");
+        HttpSession session = request.getSession();
+        String url = request.getParameter("currentAddressPage");
+        System.out.println("Context: " + url);
+        AddressDAO add = new AddressDAO();
+        int id = Integer.parseInt(request.getParameter("id"));
+        add.deleteAddress(id);
+        session.setAttribute("message", "Delete Success");
+        if (url.equalsIgnoreCase("addressPage")) {
+            response.sendRedirect("ViewShippingAddress");
+        } else if (url.equalsIgnoreCase("forOrder")) {
+            response.sendRedirect("ViewShippingAddress?action=forOrder");
+        }
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        doGet(request, response);
+        processRequest(request, response);
     }
+
+    /**
+     * Returns the servlet description.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "SMARTTICK servlet";
+    }// </editor-fold>
+
 }
